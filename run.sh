169e03dc00f08c0b37db33a7209a0d740128a650
asrafulashiq <asrafulashiq@gmail.com>
2019-09-28 22:54:20 -0400

    /

--- run.sh
+++ run.sh
@@ -31,8 +31,8 @@ python main_detect.py -m test_lamb --Lambda 0.9 --max-iter 10001 >> tmp_log/lamb
 #                 7000 --topk 60 --topk2 10 --clip 4 --dis 3.5 >> ./tmp_log/t1
 
 # ### effect of clip
-# python  main_detect.py -m thumos_base --Lambda 0.5 --max-iter \
-#                 7000 --topk 60 --topk2 10 --clip 5 --dis 3 >> ./tmp_log/t2
+python  main_detect.py -m thumos_base --Lambda 0.5 --max-iter \
+                7000 --topk 60 --topk2 10 --clip 5 --dis 3 >> ./tmp_log/t2
 # python  main_detect.py -m thumos_basetest --Lambda 0.5 --max-iter \
 #                 7000 --topk 60 --topk2 10 --clip 4 --dis 3 >> ./tmp_log/t2
 # python  main_detect.py -m thumos_basetest --Lambda 0.5 --max-iter \
